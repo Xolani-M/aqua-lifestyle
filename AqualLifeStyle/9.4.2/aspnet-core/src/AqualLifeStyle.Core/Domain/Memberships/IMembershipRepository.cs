630572e7f2150076d67ea2b8697461e1843d3ea9
Xolani-M <nathanielmvana@gmail.com>
2026-06-23 17:55:25 +0200

    fix(domain-membership): deduplicate IMembershipRepository declarations

--- AqualLifeStyle/9.4.2/aspnet-core/src/AqualLifeStyle.Core/Domain/Memberships/IMembershipRepository.cs
+++ AqualLifeStyle/9.4.2/aspnet-core/src/AqualLifeStyle.Core/Domain/Memberships/IMembershipRepository.cs
@@ -1,19 +1,12 @@
 using System.Threading.Tasks;
-
-namespace AqualLifeStyle.Domain.Memberships
-{
-    public interface IMembershipRepository
-    {
-        Task<bool> ExistsByNameAsync(string name);
-        Task<Membership?> GetByIdAsync(int id);
-        Task AddAsync(Membership membership);
-    }
-}
 using Abp.Domain.Repositories;
 
 namespace AqualLifeStyle.Domain.Memberships
 {
     public interface IMembershipRepository : IRepository<Membership, int>
     {
+        Task<bool> ExistsByNameAsync(string name);
+        Task<Membership?> GetByIdAsync(int id);
+        Task AddAsync(Membership membership);
     }
 }
